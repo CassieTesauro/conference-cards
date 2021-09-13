a40e605cf0c98ab438dc0a44c5c620a8438b9d30
CassieTesauro <cassie.tesauro@gmail.com>
2021-09-13 13:51:41 -0500

    heading and all student names displaying

--- src/components/ConferenceCards.js
+++ src/components/ConferenceCards.js
@@ -1,32 +1,30 @@
-import React from "react" 
-import { Route, Redirect } from "react-router-dom";
-import { ApplicationViews } from "./ApplicationViews";
-import { NavBar } from "./nav/NavBar";
-import { Login } from "./auth/Login";
-import { Register } from "./auth/Register";
+import React, { useEffect, useState } from "react"
 
-export const ConferenceCards = () => (
-    <>
-      <Route
-        render={() => { //every route has a custom rendering function that you can specify if you need to
-          if (localStorage.getItem("honey_customer")) {  // matches up with code in register.js.   if the customer has logged in, then the honey_customer key is present. return navbar and application views.
-            return (
-              <>
-                <NavBar />
-                <ApplicationViews />
-              </>
-            );
-          } else {
-            return <Redirect to="/login" />;  //Redirect component has been imported from the react-router-dom.  if the honey_customer key isn't in local storage, then no one has signed in. The redirect will change the url to include /login at the end, which will match the  <Route path ="/login"> below, which will invoke the <Login /> component and render the login page.
-          }
-        }}
-      />
-  
-      <Route path="/login"> 
-        <Login />
-      </Route>
-      <Route path="/register">
-        <Register />
-      </Route>
-    </>
-  );
+export const ConferenceCards = () => {
+    
+    const [students, setStudents] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/students")
+            .then(response => response.json())
+            .then((studentAPIArray) => {
+                setStudents(studentAPIArray)
+            })
+        },
+        []
+    )
+
+    return (
+        <>
+            <h1>Conference Cards</h1>
+            {
+                students.map(
+                    (studentObject) => {
+                        return <h4>{studentObject.name}</h4>
+                    }
+                )
+            }
+        </>
+    )
+}
