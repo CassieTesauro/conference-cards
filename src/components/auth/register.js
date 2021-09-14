e58515fe3db125b3521ceaa2b129daf03d6fac15
CassieTesauro <cassie.tesauro@gmail.com>
2021-09-14 11:00:33 -0500

    added conditional to StudentList.js so only current teacher roster is rendered

---
+++ src/components/auth/register.js
@@ -0,0 +1,75 @@
+import React, { useRef, useState } from "react"
+import { useHistory } from "react-router-dom"
+import "./login.css"
+
+export const Register = (props) => {
+    const [teacher, setTeacher] = useState({})
+    const conflictDialog = useRef()
+
+    const history = useHistory()
+
+    const existingUserCheck = () => {
+        return fetch(`http://localhost:8088/teachers?email=${teacher.email}`)
+            .then(res => res.json())
+            .then(user => !!user.length)
+    }
+    const handleRegister = (e) => {
+        e.preventDefault()
+        existingUserCheck()
+            .then((userExists) => {
+                if (!userExists) {
+                    fetch("http://localhost:8088/teachers", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(teacher)
+                    })
+                        .then(res => res.json())
+                        .then(createdUser => {
+                            if (createdUser.hasOwnProperty("id")) {
+                                localStorage.setItem("cc_teacher", createdUser.id)
+                                history.push("/")
+                            }
+                        })
+                }
+                else {
+                    conflictDialog.current.showModal()
+                }
+            })
+    }
+
+    const updateCustomer = (evt) => {
+        const copy = {...teacher}
+        copy[evt.target.id] = evt.target.value
+        setTeacher(copy)
+    }
+
+
+    return (
+        <main style={{ textAlign: "center" }}>
+            <dialog className="dialog dialog--password" ref={conflictDialog}>
+                <div>Account with that email address already exists</div>
+                <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
+            </dialog>
+
+            <form className="form--login" onSubmit={handleRegister}>
+                <h1 className="h3 mb-3 font-weight-normal">Please Register for Conference Cards</h1>
+                <fieldset>
+                    <label htmlFor="name"> Full Name </label>
+                    <input onChange={updateCustomer}
+                           type="text" id="name" className="form-control"
+                           placeholder="Enter your name" required autoFocus />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="email"> Email address </label>
+                    <input onChange={updateCustomer} type="email" id="email" className="form-control" placeholder="Email address" required />
+                </fieldset>
+                <fieldset>
+                    <button type="submit"> Register </button>
+                </fieldset>
+            </form>
+        </main>
+    )
+}
+
