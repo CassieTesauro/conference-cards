a6528f1494d6b3d9351c7daea05bd1154cdd7447
CassieTesauro <cassie.tesauro@gmail.com>
2021-09-19 15:30:07 -0500

    EditStudentForm component is fetching and storing student object form api

--- src/components/students/EditStudentForm.js
+++ src/components/students/EditStudentForm.js
@@ -3,18 +3,31 @@
 //main component shoud render form with that student's data from api
 
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 
 
 export const EditStudentForm = () => {
-    
-    const { studentId } = useParams()
+
+    const [student, viewStudent] = useState({}) //stores the individual student we fetch in the useEffect
+    const { studentId } = useParams() //studentId matches with the appview route 
+
+    useEffect(
+        () => {//function fetches student object state from api based on studentid.  make a useState to store it.
+            return fetch(`http://localhost:8088/students/${studentId}`)
+                .then(response => response.json())
+                .then((fetchedStudentData) => {
+                    viewStudent(fetchedStudentData)
+                })
+        },
+        [studentId]
+    )
+
 
     return (
         <>
-        <h2>student is {studentId}</h2>
+            <h2>Update or Delete your Conference Card for {student.name}</h2>
         </>
     )
 
